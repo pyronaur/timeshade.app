49a1136d50ff3953b5d33cba2c19e5450774b266
pyronaur <hi@pyronaur.com>
2020-11-01 10:24:36 +0200

    Cleanup

--- src/app.js
+++ src/app.js
@@ -1,11 +1,12 @@
-import createTimer from './timer'
-const $ = document.querySelector.bind(document);
+import createTimer from "./timer";
 
 const format_time = (time) => ("0" + time).slice(-2);
 const timer = createTimer(10);
 
 const startApp = () => {
-  const $time = $("#time");
+  const $time = document.querySelector("#time");
+  const $toggle = document.querySelector("#toggle");
+
   const render = (time) => {
     let seconds = format_time(time % 60);
     let minutes = format_time(Math.floor(time / 60));
@@ -13,7 +14,7 @@ const startApp = () => {
   };
 
   timer.callbacks.onTick.add(render);
-  $("#toggle").addEventListener("click", timer.toggle);
+  $toggle.addEventListener("click", timer.toggle);
   render(10);
 };
 
@@ -30,8 +31,6 @@ const startConsoleApp = () => {
   });
   consoleTimer.start();
 };
-
-
 
 startApp();
 startConsoleApp();
